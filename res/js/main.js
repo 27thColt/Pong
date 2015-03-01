9aa2f39c01e5ee0c2cb3d170300cfe7320010a2a
27thColt <d27thColt@gmail.com>
2015-03-01 15:40:12 +0800

    Added movement to players without acceleration and updated Phaser from 2.0.3 to 2.2.2

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,5 @@
 // Creates a new game
-game = new Phaser.Game(700, 1000, Phaser.AUTO, "game");
+game = new Phaser.Game(1000, 700, Phaser.AUTO, "game");
 
 //Adds a the main "play" state
 game.state.add("play", playState);
--- res/js/play.js
+++ res/js/play.js
@@ -7,23 +7,24 @@ playState = {
     },
     
     create:function() {
-        this.player1 = game.add.sprite(this.game.width - this.game.width + 5, this.game.height - this.game.height, "player1");
-        this.player2 = game.add.sprite(this.game.width - 40, this.game.height - this.game.height, "player2");
+
+        this.player1 = game.add.sprite(this.game.width - this.game.width + 5, this.game.height * 0.5, "player1");
+        this.player2 = game.add.sprite(this.game.width - 40, this.game.height * 0.5 , "player2");
 
         //Enabling ARCADE physics on boths players
-        this.game.physics.enable(this.player1, Physics.Phaser.ARCADE);
-        this.game.physics.enable(this.player2, Physics.Phaser.ARCADE);
+        this.game.physics.enable(this.player1, Phaser.Physics.ARCADE);
+        this.game.physics.enable(this.player2, Phaser.Physics.ARCADE);
 
         //Enables/Disables ARCADE Physics features like gravity, acceleration, etc.
         this.player1.body.acceleration = true;
         this.player1.body.allowGravity = false;
-        this.player1.body.allowRotation = false;
         this.player1.body.immovable = true;
+        this.player1.body.collideWorldBounds = true;
 
         this.player2.body.acceleration = true;
         this.player2.body.allowGravity = false;
-        this.player2.body.allowRotation = false;
         this.player2.body.immovable = true;
+        this.player2.body.collideWorldBounds = true;
 
         //normal keyboard scrolling won't work
         this.game.input.keyboard.addKeyCapture([
@@ -33,9 +34,31 @@ playState = {
             Phaser.Keyboard.RIGHT,
             Phaser.Keyboard.SPACEBAR]);
 
+        //Player Acceleration when it moves
+        this.playerAcceleration = 5;
+        this.playerSpeed = 250;
+
     },
     update:function() {
-    
+        this.player1.body.maxVelocity.setTo(this.playerSpeed, this.playerSpeed);
+        this.player2.body.maxVelocity.setTo(this.playerSpeed, this.playerSpeed);
+
+        //Player 1 movement
+        if (this.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            this.player1.body.y -= this.playerAcceleration;
+
+        } else if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            this.player1.body.y += this.playerAcceleration;
+
+        };
+
+        //Player 2 movement
+        if (this.input.keyboard.isDown(Phaser.Keyboard.W)) {
+            this.player2.body.y -= this.playerAcceleration;
+
+        } else if (this.input.keyboard.isDown(Phaser.Keyboard.S)) {
+            this.player2.body.y += this.playerAcceleration;
+        };
     
     }
 
